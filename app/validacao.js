26565c64d9dd79055221d354f2a317c8c9eec29b
Leonardo Wolff <81689830+PatinhoDaShiro@users.noreply.github.com>
2023-07-18 11:55:49 -0300

    Add files via upload

--- app/validacao.js
+++ app/validacao.js
@@ -2,13 +2,24 @@ function valorValido(chute){
     const numero =+ chute
     if(chuteForInvalido(numero)){
        elementoChute.innerHTML += '<div>Valor inválido</div>'
+       if(chute == 'game over'||'Game over'||'Game Over'){
+        document.body.style.backgroundColor = "red";
+
+        document.body.innerHTML = `
+        <h2>GAME OVER</h2>
+        <h3>O numero secreto era: ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btnJogarDenovo">Jogar novamente</button>
+        `
+    }
        return
     }
     if(numeroAltoDeMais(numero)){
         elementoChute.innerHTML+=`<div>Valor invalido: Fale um numero entre ${menorValor} ou ${maiorValor}</div>`
         return
     }
+   
     if(numero === numeroSecreto){
+        document.body.style.backgroundColor = "green";
         document.body.innerHTML = `
         <h2>Você acertou!!</h2>
         <h3>O numero secreto era: ${numero}</h3>
